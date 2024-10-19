3ff10676d2d9be0ecb2250bbc959d026e7f77af1
SamudraW <mattegoda15@gmail.com>
2024-10-19 17:49:23 +0530

    up

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,28 @@ import AdminHome from "./Pages/AdminSide/AdminHome";
 import AdminView from "./Pages/Feedback/AdminView";
 import AllFeedback from "./Pages/Feedback/AllFeedback";
 import AllWasteCollections from "./Pages/Waste_Collection_Admin/AllWasteCollections";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Cancel from "./Pages/Payment/Cancel";
-
 import EditFeedback from "./Pages/Feedback/EditFeedBack";
-
 import FeedbackForm from "./Pages/Feedback/FeedbackForm";
-
 import LoginPage from "./Pages/LoginPage";
-
 import MyFeedback from "./Pages/Feedback/MyFeedback";
-
 import PaymentDetails from "./Pages/Payment/PaymentDetails";
 import PaymentManagement from "./Pages/Payment/PaymentManagement";
 import PaymentReport from "./Pages/PaymentReport";
-
 import RecordCollection from "./Pages/Waste_Collection_Admin/RecordCollection";
 import ReportNavigation from "./Pages/ReportNavigation";
-
 import ScheduleForm from "./Pages/Waste_Schedule/ScheduleForm";
 import ScheduleReport from "./Pages/ScheduleReport";
 import Schedule_Satus_Admin from "./Pages/Waste_Schedule/Schedule_Satus_Admin"
 import SignupPage from "./Pages/SignupPage";
 import Success from "./Pages/Payment/Success";
-
 import UpdateForm from "./Pages/Waste_Schedule/UpdateForm";
 import UpdateWasteCollection from "./Pages/Waste_Collection_Admin/UpdateWasteCollection";
-
-
 import ViewSchedule from "./Pages/Waste_Schedule/ViewSchedule";
-
 import Wallet from "./Pages/Payment/Wallet";
 import WasteCollectedReport from "./Pages/WasteCollectedReport";
 import Waste_Collect_User from "./Pages/Waste_Schedule/Waste_Collect_User";
-
 import UserDashboard from "./Pages/UserDashBoard";
 
 function App() {
